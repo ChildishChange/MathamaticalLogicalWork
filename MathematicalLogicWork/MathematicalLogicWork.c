00c724d4671d998af8654ca62ebc7bf00776da2c
ChildishChange <457694145@qq.com>
2018-11-07 02:42:47 +0800

    初步完成求值，现能够正确求出不含自定义联结词的公式

--- MathematicalLogicWork/MathematicalLogicWork.c
+++ MathematicalLogicWork/MathematicalLogicWork.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <wchar.h>
 #include <wctype.h>
+#include <math.h>
 
 #pragma region MACRO
 
@@ -15,7 +16,7 @@
 #define VARIABLE_NUMBER 10 //命题变元最大数量
 #define FUNCTION_NUMBER 10 //函数最大数量
 #define TRUTHTABLE_LENGTH 1024 //真值表最大长度
-#define MAXSTACKDEPTH 102400
+#define MAXSTACKDEPTH 10000
 
 #pragma endregion
 
@@ -51,13 +52,14 @@ int varIndex = 0;
 
 struct runtimeFrame
 {
-	wchar_t symbol[IDENTIFIER_LENGTH + 1];
 	int layer;
 	int type;//1 变元 2 第一类 3 自定义
+	int value;
 	int paraNumber;
-	int paraIndex[MAXPARANUM];
-}runtimeStack[MAXSTACKDEPTH];
+	wchar_t symbol[IDENTIFIER_LENGTH + 1];
+}runtimeStack[MAXSTACKDEPTH],tempStack[MAXSTACKDEPTH];
 int stackIndex = 0;
+int tempStackIndex = 0;
 
 const enum symbol //词法符号
 {
@@ -112,6 +114,9 @@ int searchFunList(wchar_t *name);
 #pragma region RUNTIMESTACK
 
 void pushConjOne(int symbolIndex,int layer, int paraNumber);
+void getTruthValue(wchar_t * name, int type, int runtimeIndex);
+void getExpTruthValue();
+void moveStack(int runtimeIndex);
 
 #pragma endregion
 
@@ -125,7 +130,8 @@ TODO:
 
 
 TODO:
-运行前需要对所有函数检查是否定义
+关于运行栈
+第一类联结词中 0和1 可以直接计算值，考虑针对不同的联结词添加不同的函数
 
 
 */
@@ -173,6 +179,8 @@ int main(int argc, char *argv[])
 		parseExpression(expression);
 		wprintf(L"%s\n", expression);
 		
+		getExpTruthValue();
+
 		funIndex--;
 		wprintf(L"Print Function Table:\n");
 		while (funIndex>=0)
@@ -198,6 +206,8 @@ int main(int argc, char *argv[])
 			wprintf(L"Num.%d symbol : %s at layer %d\n", stackIndex, runtimeStack[stackIndex].symbol, runtimeStack[stackIndex].layer);
 			stackIndex--;
 		}
+
+		
 
 		//归零四大天王
 		wmemset(expression, 0, MAXLENTH-1);
@@ -434,7 +444,7 @@ void parseLevelFiveItem(wchar_t * expression)
 		parseLevelFourItem(expression);
 		printf("current operation : EQUIVALENCE at layer %d\n",layer);
 		//第一类联结词压栈
-		pushConjOne(EQUIVALENCE, layer, 2);
+		pushConjOne(EQUIVALENCE, layer, 2,2);
 	}
 	jumpBackward();
 	printf("parseLevelFiveItem : Current out layer : %d\n", layer);
@@ -451,7 +461,7 @@ void parseLevelFourItem(wchar_t *expression)
 		parseLevelThreeItem(expression);
 		printf("current operation : IMPLICATION at layer %d\n", layer);
 		//第一类联结词压栈
-		pushConjOne(IMPLICATION, layer, 2);
+		pushConjOne(IMPLICATION, layer, 2,2);
 	}
 	jumpBackward();
 	printf("parseLevelFourItem : Current out layer : %d\n", layer);
@@ -468,7 +478,7 @@ void parseLevelThreeItem(wchar_t *expression)
 		parseLevelTwoItem(expression);
 		printf("current operation : EXCLUSIVEOR at layer %d\n", layer);
 		//第一类联结词压栈
-		pushConjOne(EXCLUSIVEOR, layer, 2);
+		pushConjOne(EXCLUSIVEOR, layer, 2,2);
 	}
 	jumpBackward();
 	printf("parseLevelThreeItem : Current out layer : %d\n", layer);
@@ -485,7 +495,7 @@ void parseLevelTwoItem(wchar_t *expression)
 		parseLevelOneItem(expression);
 		printf("current operation : OR at layer %d\n", layer);
 		//第一类联结词压栈
-		pushConjOne(OR, layer, 2);
+		pushConjOne(OR, layer, 2,2);
 	}
 	jumpBackward();
 	printf("parseLevelTwoItem : Current out layer : %d\n", layer);
@@ -502,7 +512,7 @@ void parseLevelOneItem(wchar_t *expression)
 		parseFactor(expression);
 		printf("current operation : AND at layer %d\n", layer);
 		//第一类联结词压栈
-		pushConjOne(AND, layer, 2);
+		pushConjOne(AND, layer, 2,2);
 	}
 	jumpBackward();
 	printf("parseLevelOneItem : Current out layer : %d\n", layer);
@@ -525,7 +535,7 @@ void parseFactor(wchar_t *expression)
 		parseFactor(expression);
 		printf("current operation : NOT at layer %d\n", layer);
 		//第一类联结词压栈
-		pushConjOne(NOT, layer, 1);
+		pushConjOne(NOT, layer, 1,2);
 		break;
 	case LBRACE:
 		parseLevelFiveItem(expression);
@@ -533,11 +543,11 @@ void parseFactor(wchar_t *expression)
 		break;
 	case TRUE:
 		//第一类联结词压栈
-		pushConjOne(TRUE, layer, 0);
+		pushConjOne(TRUE, layer, 0,2);
 		break;
 	case FALSE:
 		//第一类联结词压栈
-		pushConjOne(FALSE, layer, 0);
+		pushConjOne(FALSE, layer, 0,2);
 		break;
 	case IDENTIFIER:
 		wcsncpy(tempIdentifier,currentSymbol, IDENTIFIER_LENGTH + 1);
@@ -701,13 +711,181 @@ int searchFunList(wchar_t * name)
 	return -1;
 }
 
-void pushConjOne(int symbolIndex, int layer, int paraNumber)
+void pushConjOne(int symbolIndex, int layer, int paraNumber, int type)
 {
 	//第一类联结词压栈
 	runtimeStack[stackIndex].symbol[0] = symbolSet[symbolIndex];
 	runtimeStack[stackIndex].symbol[1] = L'\0';
 	runtimeStack[stackIndex].layer = layer;
-	runtimeStack[stackIndex].type = 1;
+	runtimeStack[stackIndex].type = type;
 	runtimeStack[stackIndex].paraNumber = paraNumber;
+	if (symbolIndex == 4){runtimeStack[stackIndex].value = 1;}
+	else if (symbolIndex == 5){runtimeStack[stackIndex].value = 0;}
+	else{runtimeStack[stackIndex].value = -1;}
 	stackIndex++;
+}
+
+void getTruthValue(wchar_t * name,int type,int runtimeIndex)
+{
+	//getTruthValue前要进行赋值操作，假设调用该函数时，联结词所需的变量已在对应位置
+	int _trueValueIndex = 0;
+	int _searchResult = -1;
+	int i = 0;
+	runtimeStack[runtimeIndex].layer = 0;
+	switch (type)
+	{
+	case 1:
+		runtimeStack[runtimeIndex].value = variableList[searchVarList(runtimeStack[runtimeIndex].symbol)].truthValue;
+		return;
+	case 2://第一类联结词
+		switch (name[0])
+		{
+		case L'0':
+			return;
+		case L'1':
+			return;
+		case L'∧':
+			runtimeStack[runtimeIndex].value =  runtimeStack[runtimeIndex - 2].value & runtimeStack[runtimeIndex - 1].value;	
+			moveStack(runtimeIndex);
+			return;
+		case L'∨':
+			runtimeStack[runtimeIndex].value = runtimeStack[runtimeIndex - 2].value | runtimeStack[runtimeIndex - 1].value;
+			moveStack(runtimeIndex);
+			return;
+		case L'¬':
+			runtimeStack[runtimeIndex].value = (runtimeStack[runtimeIndex - 1].value==0)?1:0;
+			moveStack(runtimeIndex);
+			return;
+		case L'⊕':
+			runtimeStack[runtimeIndex].value =  (runtimeStack[runtimeIndex - 2].value == runtimeStack[runtimeIndex - 1].value)?0:1;
+			moveStack(runtimeIndex);
+			return;
+		case L'→':
+			if (runtimeStack[runtimeIndex - 2].value == 0 ||
+				(runtimeStack[runtimeIndex - 2].value == 1 && runtimeStack[runtimeIndex - 1].value == 1))
+			{
+				runtimeStack[runtimeIndex].value = 1;
+				moveStack(runtimeIndex);
+				return;
+			}				
+			else
+			{
+				runtimeStack[runtimeIndex].value = 0;
+				moveStack(runtimeIndex);
+				return;
+			}
+				
+		case L'↔':
+			if (runtimeStack[runtimeIndex - 2].value == runtimeStack[runtimeIndex - 1].value)
+			{
+				runtimeStack[runtimeIndex].value = 1;
+				moveStack(runtimeIndex);
+				return;
+			}
+			else 
+			{
+				runtimeStack[runtimeIndex].value = 0;
+				moveStack(runtimeIndex);
+				return;
+			}
+		}
+	case 3://第二类联结词
+		_searchResult = searchFunList(name);
+		for (i = runtimeIndex - functionList[_searchResult].paraNumber;i<runtimeIndex;i++)
+		{
+			_trueValueIndex *= 2;
+			_trueValueIndex += runtimeStack[i].value;
+		}
+		runtimeStack[runtimeIndex].value = _wtoi(functionList[_searchResult].truthTable[_trueValueIndex]);
+		moveStack(runtimeIndex);
+		return;
+	}
+}
+
+void getExpTruthValue()
+{
+	//现在我们有一个运行栈
+	
+	int i = 0;
+	int j = 0;
+	int _deepest = -1;
+	int _repeatTimes = pow(2, varIndex);
+
+	//计算之前，需要复制运行栈
+	tempStackIndex = stackIndex;
+	for (j = 0; i < stackIndex; i++)
+	{
+		tempStack[i] = runtimeStack[i];
+	}
+
+	for (j = 0; j < varIndex; j++)//对命题变元赋值
+	{
+		wprintf(L"%s\t", variableList[j].name);
+	}
+	putchar('\n');
+
+	for (i = 0; i < _repeatTimes;i++)//对所有取值进行赋值
+	{
+		for (j = 0; j < varIndex; j++)//对命题变元赋值
+		{
+			variableList[j].truthValue = ((1 << (varIndex - 1 - j))& i) >> (varIndex - 1 - j);
+			printf("%d\t", variableList[j].truthValue);
+		}
+		
+		for (j = 0; j < stackIndex; j++)//将赋值带入运行栈
+		{
+			if (runtimeStack[j].type == 1)
+			{
+				getTruthValue(runtimeStack[j].symbol, 1, j);
+			}
+		}
+
+		while (stackIndex > 1)
+		{
+			for (j = 0; j < stackIndex; j++)//找到第一个最深
+			{
+				if (runtimeStack[j].layer > _deepest && runtimeStack[j].type != 1)
+				{
+					_deepest = runtimeStack[j].layer;
+				}
+			}
+			for (j = 0; j < stackIndex; j++)//执行完所有同深度的函数，修改getTruthValue可能会好些
+			{
+				if (runtimeStack[j].layer == _deepest && runtimeStack[j].type != 1)//只要不是变元就执行
+				{//bug在这个j
+					getTruthValue(runtimeStack[j].symbol, runtimeStack[j].type, j);
+					j = 0;
+				}
+			}
+			_deepest = -1;
+		}
+		printf("Truth Value:%d\n",runtimeStack[stackIndex-1].value);
+		//把运行栈复制回去
+		
+		stackIndex = tempStackIndex;
+		for (j = 0; j < tempStackIndex; j++)
+		{
+			runtimeStack[j] = tempStack[j];
+		}
+
+	}
+
+	//计算运算栈时，对每一个逻辑联结词，包括自定义联结词
+	//按照layer从大到小，按照出现的先后顺序从先到后计算运算符
+	
+
+	return 0;
+}
+
+void moveStack(int runtimeIndex)
+{
+	//TODO 还差移动，从runtimeIndex开始，往前移动paraNumber位，stackIndex减去对应数值
+	int i;
+	int len = runtimeStack[runtimeIndex].paraNumber;
+	for (i = runtimeIndex; i < stackIndex; i++)
+	{
+		runtimeStack[i - len] = runtimeStack[i];
+	}
+	stackIndex -= len;
+
 }
